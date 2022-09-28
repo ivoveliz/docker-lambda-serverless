665ebf6cad4ea39782c951ad56d521c549785e7a
Ivo Veliz saleh <102048374+ivoveliz@users.noreply.github.com>
2022-09-28 13:15:54 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import serverless_wsgi
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+def handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
